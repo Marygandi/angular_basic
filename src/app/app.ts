caedbd0f98db5894a8699b926e2513eb18bbb17a
mary <gandimary8@gmail.com>
2026-05-17 10:12:54 +0530

    created intial project

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular_basic');
+  name="Angular Tutorial";
+  isAdmin=true;
+  getMessage(){
+    return "welcome to the portal";
+  }
+  count=signal(10);
+  employee={
+    name:"mary",
+    age:24,
+    salary:30000
+  }
 }
