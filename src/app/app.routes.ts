27c983cad632fa4d53d974310722493e9e82bf88
mary <gandimary8@gmail.com>
2026-05-19 11:08:50 +0530

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { AboutComponent } from './about-component/about-component';
+import { ContactComponent } from './contact-component/contact-component';
+import { Profile } from './HomeComponent/profile/profile';
+import { Settings } from './HomeComponent/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path:'home',component:HomeComponent,
+    children:[
+        {path:'profile',component:Profile},
+{path:'settings',component:Settings}
+    ]
+    },
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent}
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, effect, signal, } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ShortNamePipe } from './short-name-pipe';
 import { FormControl, FormControlName, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Child } from './child/child';
+import { routes } from './app.routes';
 interface user{
   name: string,
   age:number
@@ -11,35 +11,14 @@ interface user{
 }
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,ShortNamePipe,FormsModule,Child,ReactiveFormsModule],
+  imports: [RouterOutlet,RouterLink,CommonModule,ShortNamePipe,FormsModule,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
   protected readonly title = signal('angular_basic');
-  username=signal('mary gandi');
-userid=signal<string>('mary');
-
-// forms pratctise-
-// Ng model---template driven form practise
-submit(f:any){
-console.log(f.value);
-}
-//reactive form 
-form=new FormGroup({
-  Name:new FormControl(''),
-  Email:new FormControl('')
-});
-submitReactive(){
-  console.log(this.form.value);
-}
-// singals form
-employeename=signal<string>('');
-employeemail=signal<string>('');
-submitSignalForm(){
-  console.log(this.employeename(),this.employeemail());
-}
+ 
 
   }
 
